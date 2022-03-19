6b87d5105c237bffa00ce96e1f4a32f0c63f11d5
Leo Becker <leobecker@Leos-MacBook-Pro.local>
2022-03-19 08:55:02 +0100

    started testing apple document packages

--- src/Core/entry.cpp
+++ src/Core/entry.cpp
@@ -21,7 +21,6 @@ int main() {
 	//Logger is initialized after working directory is defined to have log file access
 	Logger::init();
 		
-	/*
 	Logger::info("Stacato Version {}.{} {} - "
 #ifdef STACATO_DEBUG
 				 "Debug Build",
@@ -29,7 +28,7 @@ int main() {
 				 "Release Build",
 #endif
 				 VERSION_MAJOR, VERSION_MINOR, STACATO_OS_NAME);
-	*/
+	 
 	Logger::debug("Application Working Directory: {}", std::filesystem::current_path().string());
 	
 	//initialize node factory librairies
