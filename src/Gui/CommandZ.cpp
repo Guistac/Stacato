9b872f1bea809c46f42648b417166d323094a131
Leo Becker <leobecker@Leos-MacBook-Pro.local>
2022-05-03 13:44:48 +0200

    testing Command Pattern and undo history

--- src/Gui/CommandZ.cpp
+++ src/Gui/CommandZ.cpp
@@ -2,93 +2,91 @@
 
 #include <imgui.h>
 
-namespace ActionHistory{
+namespace CommandHistory{
 
-	std::vector<std::shared_ptr<Action>> history;
-	size_t undoableActionCount = 0;
+	std::vector<std::shared_ptr<Command>> history;
+	size_t undoableCommandCount = 0;
 
-	void push(std::shared_ptr<Action> action){
-		if(undoableActionCount < history.size()) {
-			history.erase(history.begin() + undoableActionCount, history.end());
-		}
-		history.push_back(action);
-		undoableActionCount++;
+	void push(std::shared_ptr<Command> command){
+		if(undoableCommandCount < history.size()) history.erase(history.begin() + undoableCommandCount, history.end());
+		history.push_back(command);
+		undoableCommandCount++;
 	}
 
 	void undo(){
-		undoableActionCount--;
-		history[undoableActionCount]->undo();
+		undoableCommandCount--;
+		history[undoableCommandCount]->undo();
 	}
 
 	void redo(){
-		history[undoableActionCount]->redo();
-		undoableActionCount++;
+		history[undoableCommandCount]->execute();
+		undoableCommandCount++;
 	}
 
 
 	bool canUndo(){
-		return undoableActionCount > 0;
+		return undoableCommandCount > 0;
 	}
 
 	bool canRedo(){
-		return undoableActionCount < history.size();
+		return undoableCommandCount < history.size();
 	}
 
-	std::vector<std::shared_ptr<Action>>& get(){
+	std::vector<std::shared_ptr<Command>>& get(){
 		return history;
 	}
 
 	size_t getTopIndex(){
-		return undoableActionCount;
+		void (*test)() = nullptr;
+		return undoableCommandCount;
 	}
 	
 };
 
 
 
+
+
+
 class DoubleParameter : public std::enable_shared_from_this<DoubleParameter>{
 public:
 	
-	class EditAction : public Action{
+	class EditValueCommand : public Command{
 	public:
-		
 		std::shared_ptr<DoubleParameter> parameter;
-		double value, previousValue;
-		
-		void set(std::shared_ptr<DoubleParameter> parameter_){
+		double newValue;
+		double previousValue;
+		EditValueCommand(std::shared_ptr<DoubleParameter> parameter_){
 			parameter = parameter_;
-			value = parameter->value;
+			newValue = parameter->value;
 			previousValue = parameter->previousValue;
-			name = "Changed " + std::string(parameter->name) + " from " + std::to_string(previousValue) + " to " + std::to_string(value);
+			name = "Changed " + std::string(parameter->name) + " from " + std::to_string(previousValue) + " to " + std::to_string(newValue);
 		}
-		
+		virtual void execute(){
+			parameter->value = newValue;
+			parameter->previousValue = newValue;
+			parameter->b_changed = true;
+		}
 		virtual void undo(){
 			parameter->value = previousValue;
 			parameter->previousValue = previousValue;
 			parameter->b_changed = true;
 		}
-		virtual void redo(){
-			parameter->value = value;
-			parameter->previousValue = value;
-			parameter->b_changed = true;
-		}
 	};
 	
-	DoubleParameter(double value_, const char* name_) : value(value_), previousValue(value_), name(name_){}
+	DoubleParameter(double value_, std::string name_) : value(value_), previousValue(value_), name(name_){}
 	
 	void gui(){
-		ImGui::SetNextItemWidth(200.0);
-		ImGui::InputDouble(name, &value);
+		ImGui::SetNextItemWidth(100.0);
+		ImGui::InputDouble(name.c_str(), &value);
 		if(ImGui::IsItemDeactivatedAfterEdit() && previousValue != value){
-			Logger::warn("{} Modified from {} to {}", name, previousValue, value);
 			
-			std::shared_ptr<EditAction> action = std::make_shared<EditAction>();
-			std::shared_ptr<DoubleParameter> thisParameter = shared_from_this();
-			action->set(thisParameter);
-			ActionHistory::push(action);
+			//=========Command Invoker=========
+			std::shared_ptr<EditValueCommand> action = std::make_shared<EditValueCommand>(shared_from_this());
+			action->execute();
+			CommandHistory::push(action);
+			//=================================
 			
-			previousValue = value;
-			b_changed = true;
 		}
 	}
 	
@@ -101,43 +99,104 @@ public:
 	bool b_changed = false;
 	double previousValue = 0.0;
 	double value = 0.0;
-	const char* name = "default parameter name";
+	std::string name = "default parameter name";
 	
 };
 
+
+
+class AddRemoveParameterCommand : public Command{
+public:
+	std::shared_ptr<DoubleParameter> parameter;
+	std::vector<std::shared_ptr<DoubleParameter>>* list;
+	int index;
+	bool b_add;
+	
+	AddRemoveParameterCommand(std::shared_ptr<DoubleParameter> parameter_, std::vector<std::shared_ptr<DoubleParameter>>* list_, int index_, bool add_){
+		parameter = parameter_;
+		list = list_;
+		b_add = add_;
+		index = index_;
+		if(b_add) name = "Added Parameter " + parameter->name + " at index " + std::to_string(index);
+		else name = "Removed Parameter " + parameter->name + " at index " + std::to_string(index);
+	}
+	
+	void add(){
+		list->insert(list->begin() + index, parameter);
+	}
+	
+	void remove(){
+		list->erase(list->begin() + index);
+	}
+	
+	virtual void execute(){
+		if(b_add) add();
+		else remove();
+	}
+	
+	virtual void undo(){
+		if(b_add) remove();
+		else add();
+	}
+};
+
+
+
+
+
+
 void testUndoHistory(){
 	
-	static std::shared_ptr<DoubleParameter> param1 = std::make_shared<DoubleParameter>(1.0, "parameter 1");
-	static std::shared_ptr<DoubleParameter> param2 = std::make_shared<DoubleParameter>(2.0, "parameter 2");
-	static std::shared_ptr<DoubleParameter> param3 = std::make_shared<DoubleParameter>(3.0, "parameter 3");
-	static std::shared_ptr<DoubleParameter> param4 = std::make_shared<DoubleParameter>(4.0, "parameter 4");
-	static std::shared_ptr<DoubleParameter> param5 = std::make_shared<DoubleParameter>(5.0, "parameter 5");
+	static std::vector<std::shared_ptr<DoubleParameter>> parameters = {
+		std::make_shared<DoubleParameter>(1.0, "parameter 0"),
+		std::make_shared<DoubleParameter>(2.0, "parameter 1"),
+		std::make_shared<DoubleParameter>(3.0, "parameter 2"),
+		std::make_shared<DoubleParameter>(4.0, "parameter 3"),
+		std::make_shared<DoubleParameter>(5.0, "parameter 4")
+	};
 	
 	static const char* lastModifiedParameterName = "";
 	
-	ImGui::BeginDisabled(!ActionHistory::canUndo());
-	if(ImGui::Button("Undo")) ActionHistory::undo();
+	ImGui::BeginDisabled(!CommandHistory::canUndo());
+	if(ImGui::Button("Undo")) CommandHistory::undo();
 	ImGui::EndDisabled();
 	ImGui::SameLine();
-	ImGui::BeginDisabled(!ActionHistory::canRedo());
-	if(ImGui::Button("Redo")) ActionHistory::redo();
+	ImGui::BeginDisabled(!CommandHistory::canRedo());
+	if(ImGui::Button("Redo")) CommandHistory::redo();
 	ImGui::EndDisabled();
 	
-	param1->gui();
-	if(param1->changed()) lastModifiedParameterName = param1->name;
-	param2->gui();
-	if(param2->changed()) lastModifiedParameterName = param2->name;
-	param3->gui();
-	if(param3->changed()) lastModifiedParameterName = param3->name;
-	param4->gui();
-	if(param4->changed()) lastModifiedParameterName = param4->name;
-	param5->gui();
-	if(param5->changed()) lastModifiedParameterName = param5->name;
+	
+	int deletedParameterIndex = -1;
+	
+	for(int i = 0; i < parameters.size(); i++){
+		auto& parameter = parameters[i];
+		ImGui::PushID(i);
+		if(ImGui::Button("X")) deletedParameterIndex = i;
+		ImGui::SameLine();
+		parameter->gui();
+		if(parameter->changed()) lastModifiedParameterName = parameter->name.c_str();
+		ImGui::PopID();
+	}
+	
+	if(deletedParameterIndex >= 0){
+		auto& deletedParameter = parameters[deletedParameterIndex];
+		auto command = std::make_shared<AddRemoveParameterCommand>(deletedParameter, &parameters, deletedParameterIndex, false);
+		command->execute();
+		CommandHistory::push(command);
+	}
+	
+	if(ImGui::Button("+")){
+		std::string name = "parameter " + std::to_string(parameters.size());
+		auto newParameter = std::make_shared<DoubleParameter>(0.0, name);
+		auto command = std::make_shared<AddRemoveParameterCommand>(newParameter, &parameters, parameters.size(), true);
+		command->execute();
+		CommandHistory::push(command);
+	}
 	
 	if(ImGui::BeginListBox("History")){
 		
-		int topIndex = ActionHistory::getTopIndex();
-		std::vector<std::shared_ptr<Action>>& history = ActionHistory::get();
+		int topIndex = CommandHistory::getTopIndex();
+		std::vector<std::shared_ptr<Command>>& history = CommandHistory::get();
 		bool b_disabled = false;
 		
 		for(int i = 0; i < history.size(); i++){
--- src/Gui/CommandZ.h
+++ src/Gui/CommandZ.h
@@ -1,24 +1,26 @@
 #pragma once
 
 
-class Action{
+class Command{
 public:
+	virtual void execute() = 0;
 	virtual void undo() = 0;
-	virtual void redo() = 0;
 	std::string name;
 };
 
-namespace ActionHistory{
+namespace CommandHistory{
 
-	void push(std::shared_ptr<Action> action);
+	void push(std::shared_ptr<Command> command);
 	void undo();
 	void redo();
 	bool canUndo();
 	bool canRedo();
-	std::vector<std::shared_ptr<Action>>& get();
+
+	std::vector<std::shared_ptr<Command>>& get();
 	size_t getTopIndex();
-	
 };
+
+
 
 
 
