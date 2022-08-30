f07270e312c0015152bc88873b5ab8a0ff30a7ea
Leo Becker <leobecker3@gmail.com>
2022-08-30 12:20:49 +0200

    more gui scale changes

--- src/Gui/ApplicationWindow/ApplicationWindow.cpp
+++ src/Gui/ApplicationWindow/ApplicationWindow.cpp
@@ -140,21 +140,17 @@ namespace ApplicationWindow {
 		io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
 		io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
 		
-        
-        float displayScale;
-        glfwGetWindowContentScale(window, &displayScale, nullptr);
-        float guiScale;
-        
+                
         #ifdef STACATO_WIN32
-        guiScale = 1.25 * displayScale; //best scaling to match Windows Gui
+		float displayScale;
+		glfwGetWindowContentScale(window, &displayScale, nullptr);
+		float guiScale = 1.25f * displayScale; //best scaling to match Windows Gui
         #endif
         #ifdef STACATO_MACOS
-        if(displayScale == 1.0) guiScale = 1.4;      //best scaling to match non retina MacOS Gui
-        else if(displayScale == 2.0) guiScale = 0.7; //best scaling to match retina MacOS Gui
-        else guiScale = 1.0;
+		float guiScale = 1.4f; //best scaling to match MacOS Gui
         #endif
         
-		Logger::debug("Display Scale: {} Gui Scale: {}", displayScale, guiScale);
+		Logger::debug("Gui Scale: {}", guiScale);
 		Fonts::load(guiScale);
 		Images::load();
 		
