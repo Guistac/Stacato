76bb984ff65ed7c02ae339f31feeb89823c8e8a9
Stacato Starmania <stacatostarmania@Stacato-Starmania.local>
2022-09-02 18:50:56 +0200

    changed console mapping, fixed dispatch security

--- src/Console/Consoles/ConsoleStarmaniaGui.cpp
+++ src/Console/Consoles/ConsoleStarmaniaGui.cpp
@@ -8,17 +8,19 @@
 void ConsoleStarmania::gui(float height){
 	ImDrawList* drawing = ImGui::GetWindowDrawList();
 	
-	float joystickWidth = ImGui::GetTextLineHeight() * .3f;
+	float joystickWidth = ImGui::GetTextLineHeight() * .2f;
+    float joystickTipSize = ImGui::GetTextLineHeight() * .5f;
 	
 	auto drawJoystick = [&](std::shared_ptr<Joystick2X> joystick){
 		ImGui::InvisibleButton("joystick1", glm::vec2(height));
 		glm::vec2 min = ImGui::GetItemRectMin();
 		glm::vec2 max = ImGui::GetItemRectMax();
-		glm::vec2 center = (max - min) / 2.0;
+		glm::vec2 center = (max + min) / 2.0;
+        glm::vec2 joystickEndPosition = center + joystick->getPosition() * glm::vec2(height * .4f) * glm::vec2(1.f, -1.f);
 		drawing->AddRectFilled(min, max, ImColor(Colors::darkGray), ImGui::GetStyle().FrameRounding, ImDrawFlags_RoundCornersAll);
-		drawing->AddCircleFilled(center, joystickWidth * 2.f, ImColor(Colors::almostBlack));
-		drawing->AddLine(center, center + glm::vec2(height * .5f) * joystickLeft->getPosition(), ImColor(Colors::almostBlack));
-		drawing->AddCircleFilled(center + glm::vec2(height * .5f) * joystickLeft->getPosition(), joystickWidth * 3.f, ImColor(Colors::darkRed));
+        drawing->AddCircleFilled(center, joystickWidth * .75f, ImColor(Colors::almostBlack));
+		drawing->AddLine(center, joystickEndPosition, ImColor(Colors::almostBlack), joystickWidth);
+		drawing->AddCircleFilled(joystickEndPosition, joystickTipSize * .5f, joystick->getPosition() == glm::vec2(0.f) ? ImColor(Colors::blue) : ImColor(Colors::darkRed));
 	};
 	
 	drawJoystick(joystickLeft);
