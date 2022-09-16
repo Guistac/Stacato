06b80c2004b4aa8318eaf2be267cea413e2a0880
Leo Becker <leobecker3@gmail.com>
2022-09-16 20:27:00 +0200

    improved stage visualizer

--- dir/Resources/DebugProject.stacato/Scripts/VisualizerScript/Visualizer.lua
+++ dir/Resources/DebugProject.stacato/Scripts/VisualizerScript/Visualizer.lua
@@ -4,7 +4,7 @@ local Visualizer = {}
 
 
 local lameVideoFace
-local lameVideoMilieu
+local lameVideoLointain
 local periacteLointainJardin
 local periacteLointainCour
 local periacteMilieuJardin
@@ -56,6 +56,8 @@ local flipOpeningImage = of.Image()
 local flipOpenImage = of.Image()
 local flipRaisingImage = of.Image()
 local flipRaisedImage = of.Image()
+local tournetteCentreImage = of.Image()
+local tournetteAnneauImage = of.Image()
 
 
 function Visualizer.setup()
@@ -67,9 +69,11 @@ function Visualizer.setup()
     flipOpenImage:load("Images/FlipOpen.png")
     flipRaisingImage:load("Images/FlipRaising.png")
     flipRaisedImage:load("Images/FlipRaised.png")
+    tournetteCentreImage:load("Images/TournetteCentre.png")
+    tournetteAnneauImage:load("Images/TournetteAnneau.png")
     
     lameVideoFace =             Environnement.getMachine("Lames Vidéo"):getAnimatable("Face");
-    lameVideoMilieu =           Environnement.getMachine("Lames Vidéo"):getAnimatable("Lointain");
+    lameVideoLointain =           Environnement.getMachine("Lames Vidéo"):getAnimatable("Lointain");
     periacteLointainJardin =    Environnement.getMachine("Périactes Lointain"):getAnimatable("Jardin");
     periacteLointainCour =      Environnement.getMachine("Périactes Lointain"):getAnimatable("Cour");
     periacteMilieuJardin =      Environnement.getMachine("Périactes Milieu"):getAnimatable("Jardin");
@@ -112,11 +116,17 @@ function Visualizer.setup()
 end
 
 
-local drawingMinX = -11520
-local drawingMinY = -8562.5
-local drawingMaxX = 11520
-local drawingMaxY = 8562.5
-local drawingMargin = 500.0
+--local drawingMinX = -11520
+--local drawingMinY = -8562.5
+--local drawingMaxX = 11520
+--local drawingMaxY = 8562.5
+--local drawingMargin = 500.0
+
+local drawingMinX = -14520
+local drawingMinY = -10438
+local drawingMaxX = 14520
+local drawingMaxY = 10712
+local drawingMargin = 200.0
 
 drawingMinX = drawingMinX - drawingMargin
 drawingMinY = drawingMinY - drawingMargin
@@ -129,7 +139,10 @@ local aspectRatio = drawingSizeX / drawingSizeY
 
 
 function drawFlip(minX, minY, sizeX, sizeY, flipCount, flipState, bright)
-    if bright then of.setColor(100) else of.setColor(120) end
+
+    of.setRectMode(of.RECTMODE_CORNER)
+
+    if bright then of.setColor(40) else of.setColor(100) end
     of.drawRectangle(minX, minY, sizeX, sizeY)
 
     local flipSizeX = sizeX / flipCount
@@ -150,6 +163,84 @@ function drawFlip(minX, minY, sizeX, sizeY, flipCount, flipState, bright)
     end
 
 end
+
+function drawCostiere(posX, zeroY, rangeY, animatable)
+    local position_meters = animatable:getActualValue().Position
+    local posY = zeroY + position_meters * 1000
+    local sizeX = 3150
+    local sizeY = 4150
+    local lineWidth = 50
+    of.setRectMode(of.RECTMODE_CORNER)
+    of.setColor(255, 255, 255, 63)
+    of.drawRectangle(posX - lineWidth * 0.5, zeroY, lineWidth, rangeY)
+
+    of.setRectMode(of.RECTMODE_CENTER)
+    of.setColor(255, 255, 255, 200)
+    of.drawRectangle(posX, posY, sizeX, sizeY)
+    of.setColor(0)
+    of.drawRectangle(posX, posY, 200, 200)
+    --of.drawCircle(posX, posY, 100, 100)
+end
+
+function periacteGraphic(x, y, mirror, anchorX, anchorY)
+    local sizeX = 1341.08
+    local sizeY = 577.89
+    x = x - anchorX * sizeX
+    y = y - anchorY * sizeY
+    of.pushMatrix()
+    if(mirror) then
+        of.scale(-1, 1)
+        of.translate(-x - sizeX, y)
+    else
+        of.translate(x, y)
+    end
+    of.setColor(255, 255, 255, 100)
+    of.beginShape()
+    of.vertex(72.56, 0)
+    of.vertex(291.54, 471.64)
+    of.vertex(1307.39, 0)
+    of.endShape()
+    of.setColor(0)
+    of.beginShape()
+    of.vertex(0, 33.69)
+    of.vertex(72.56, 0)
+    of.vertex(291.54, 471.64)
+    of.vertex(1307.39, 0)
+    of.vertex(1341.08, 72.56)
+    of.vertex(252.67, 577.89)
+    of.endShape()
+    of.popMatrix()
+end
+
+function drawPeriactes(posY, rangeX, animatable1, animatable2, orientation1, orientation2)
+    local lineThickness = 50
+    of.setColor(255, 255, 255, 63)
+    of.setRectMode(of.RECTMODE_CENTER)
+    of.drawRectangle(0, posY, rangeX * 2, lineThickness)
+    local pos1 = animatable1:getActualValue().Position
+    local pos2 = animatable2:getActualValue().Position
+    periacteGraphic(pos1, posY, orientation1, 1, 0.5)
+    periacteGraphic(pos2, posY, orientation2, 0, 0.5)
+end
+
+function drawLames(posY, rangeX, animatable)
+    local lineThickness = 50
+    of.setColor(255, 255, 255, 63)
+    of.setRectMode(of.RECTMODE_CENTER)
+    of.drawRectangle(0, posY, rangeX * 2, lineThickness)
+
+    local posX = animatable:getActualValue().Position
+    local lameThickness = 100
+    local rectY = posY - lameThickness * 0.5
+    of.setColor(0)
+    of.setRectMode(of.RECTMODE_CORNER)
+    of.drawRectangle(posX, rectY, 1000, lameThickness)
+    of.drawRectangle(posX + 2000, rectY, 1000, lameThickness)
+    of.drawRectangle(- 1000 - posX, rectY, 1000, lameThickness)
+    of.drawRectangle(- 3000 - posX, rectY, 1000, lameThickness)
+end
+
+
 
 
 
@@ -181,10 +272,52 @@ function Visualizer.draw()
 
 end
 
+local counter = 0
+
 function drawStage()
 
-    of.setColor(0,0,0,64)
-    of.drawRectangle(drawingMinX, drawingMinY, drawingSizeX, drawingSizeY)
+    --of.setColor(0,0,0,64)
+    --of.drawRectangle(drawingMinX, drawingMinY, drawingSizeX, drawingSizeY)
+
+    of.setColor(64)
+    of.beginShape()
+
+    of.vertex(-14520,8519)
+    of.vertex(-13020,8519)
+    of.vertex(-13020,9419)
+    of.vertex(-11520,9702)
+    of.vertex(-10080,9940)
+    of.vertex(-8640,10145)
+    of.vertex(-7200,10319)
+    of.vertex(-5760,10460)
+    of.vertex(-4320,10570)
+    of.vertex(-2880,10649)
+    of.vertex(-1440,10696)
+    of.vertex(0,10712)
+    of.vertex(1440,10696)
+    of.vertex(2880,10649)
+    of.vertex(4320,10570)
+    of.vertex(5760,10460)
+    of.vertex(7200,10319)
+    of.vertex(8640,10145)
+    of.vertex(10080,9940)
+    of.vertex(11520,9702)
+    of.vertex(13020,9419)
+    of.vertex(13020,8519)
+    of.vertex(14520,8519)
+
+    of.vertex(14520, 8562)
+    of.vertex(14520, -10438)
+    of.vertex(-14520, -10438)
+    of.vertex(-14520, 8562)
+    
+
+    of.endShape()
+
+    of.setRectMode(of.RECTMODE_CENTER)
+    of.setColor(0)
+    of.drawRectangle(0, -2638, 29040, 50)
+    of.drawRectangle(0, -2338, 29040, 50)
 
     --flips row G
     drawFlip(-8480, -1438, 3960, 1000, 4, Flip_GJ2, true)
@@ -194,16 +327,16 @@ function drawStage()
     drawFlip(4520, -1438, 3960, 1000, 4, Flip_GC2, true)
 
     --flips row E
-    drawFlip(-11520, 1562, 3040, 1000, 3, Flip_EJ3, true)
+    drawFlip(-9493, 1562, 1013, 1000, 1, Flip_EJ3, true)
     drawFlip(-8480, 1562, 3960, 1000, 4, Flip_EJ2, false)
     drawFlip(4520, 1562, 3960, 1000, 4, Flip_EC2, true)
-    drawFlip(8480, 1562, 3040, 1000, 3, Flip_EC3, false)
+    drawFlip(8480, 1562, 1013, 1000, 1, Flip_EC3, false)
 
     --flips row C
-    drawFlip(-11520, 4562, 3040, 1000, 3, Flip_CJ3, false)
+    drawFlip(-10506, 4562, 2026, 1000, 2, Flip_CJ3, false)
     drawFlip(-8480, 4562, 3960, 1000, 4, Flip_CJ2, true)
     drawFlip(4520, 4562, 3960, 1000, 4, Flip_CC2, false)
-    drawFlip(8480, 4562, 3040, 1000, 3, Flip_CC3, true)
+    drawFlip(8480, 4562, 2026, 1000, 2, Flip_CC3, true)
 
     --flips row A
     drawFlip(-11520, 7562, 3040, 1000, 3, Flip_AJ3, true)
@@ -215,20 +348,46 @@ function drawStage()
     drawFlip(8480, 7562, 3040, 1000, 3, Flip_AC3, true)
 
     --tournettes
+    --[[
     of.setRectMode(of.RECTMODE_CENTER)
     of.setCircleResolution(64)
     of.setColor(200)
     of.drawCircle(0, 3563, 3800) -- center & radius
     of.setColor(255)
     of.drawCircle(0, 3563, 2800) -- center & radius
+    ]]--
+
+    counter = counter + 1
+
+
+    of.setRectMode(of.RECTMODE_CENTER)
+    of.pushMatrix()
+    of.translate(0, 3563)
+    of.rotateZDeg(counter)
+    tournetteCentreImage:draw(0, 0, 7600, 7600)
+    of.popMatrix()
+
+    of.pushMatrix()
+    of.translate(0, 3563)
+    of.rotateZDeg(-counter)
+    tournetteAnneauImage:draw(0, 0, 7600, 7600)
+    of.popMatrix()
+
 
     --costieres
-    of.setColor(255)
-    of.drawLine(-6510, -7733, -6510, 4487)
-    of.drawLine(6510, -7733, 6510, 4487)
-    
+    drawCostiere(-6510, -7640, 12100, costiereJardin)
+    drawCostiere(6510, -7640, 12100, costiereCour)
+
+    drawPeriactes(0, 12850, periacteFaceCour, periacteFaceJardin, true, false)
+    drawPeriactes(3000, 12850, periacteFaceCour, periacteFaceJardin, true, true)
+    drawPeriactes(6002, 12850, periacteFaceCour, periacteFaceJardin, false, false)
+
+    drawLames(4262, 11200, lameVideoLointain)
+    drawLames(4462, 11200, lameVideoFace)
+
     --center cross hair
     --[[
+    of.setRectMode(of.RECTMODE_CORNER)
     of.setColor(255,0,0)
     of.drawRectangle(-100,-100,100,100)
     of.drawRectangle(0,0,100,100)
