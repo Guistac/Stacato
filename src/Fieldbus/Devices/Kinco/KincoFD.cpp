5bb22834c9b255a5658a34e6a75b7cedca33ee20
Leo Becker <leobecker3@gmail.com>
2023-03-30 11:07:12 +0200

    started testing kinco FD motion

--- src/Fieldbus/Devices/Kinco/KincoFD.cpp
+++ src/Fieldbus/Devices/Kinco/KincoFD.cpp
@@ -1,10 +1,66 @@
 #include "KincoFD.h"
 
+#include "Fieldbus/EtherCatFieldbus.h"
+
 void KincoFD::onDisconnection() {}
 void KincoFD::onConnection() {}
-void KincoFD::initialize() {}
-bool KincoFD::startupConfiguration() { return true; }
-void KincoFD::readInputs() {}
-void KincoFD::writeOutputs(){}
+
+void KincoFD::initialize() {
+	auto thisDevice = std::static_pointer_cast<EtherCatDevice>(shared_from_this());
+	axis = DS402Axis::make(thisDevice);
+	
+	axis->processDataConfiguration.enableCyclicSynchronousPositionMode();
+	//axis->processDataConfiguration.enableFrequencyMode();
+	//axis->processDataConfiguration.enableCyclicSynchronousVelocityMode();
+	//axis->processDataConfiguration.positionFollowingErrorActualValue = true;
+	axis->processDataConfiguration.errorCode = true;
+	//axis->processDataConfiguration.currentActualValue = true;
+	
+	rxPdoAssignement.addNewModule(0x1600);
+	txPdoAssignement.addNewModule(0x1A00);
+	axis->configureProcessData();
+}
+
+bool KincoFD::startupConfiguration() {
+	//———— PDO Assignement
+
+	if(!rxPdoAssignement.mapToRxPdoSyncManager(getSlaveIndex())) return false;
+	if(!txPdoAssignement.mapToTxPdoSyncManager(getSlaveIndex())) return false;
+	
+	//———— Synchronisation
+	
+	uint32_t cycleTime_nanoseconds = EtherCatFieldbus::processInterval_milliseconds * 1'000'000;
+	uint32_t shiftTime_nanoseconds = EtherCatFieldbus::processInterval_milliseconds * 500'000;
+	ec_dcsync0(getSlaveIndex(), true, cycleTime_nanoseconds, shiftTime_nanoseconds);
+	
+	return true;
+}
+void KincoFD::readInputs() {
+	txPdoAssignement.pullDataFrom(identity->inputs);
+	axis->updateInputs();
+	
+	
+	
+}
+void KincoFD::writeOutputs(){
+	
+	if(b_enable){
+		b_enable = false;
+		axis->enable();
+	}
+	
+	if(b_disable){
+		b_disable = false;
+		axis->disable();
+	}
+	
+	axis->setOperatingMode(DS402Axis::OperatingMode::CYCLIC_SYNCHRONOUS_POSITION);
+	axis->setPosition(pos);
+	
+	axis->updateOutput();
+	rxPdoAssignement.pushDataTo(identity->outputs);
+}
+
+
 bool KincoFD::saveDeviceData(tinyxml2::XMLElement* xml) { return true; }
 bool KincoFD::loadDeviceData(tinyxml2::XMLElement* xml) { return true; }
--- src/Fieldbus/Devices/Kinco/KincoFD.h
+++ src/Fieldbus/Devices/Kinco/KincoFD.h
@@ -1,11 +1,17 @@
 #pragma once
 
 #include "Fieldbus/EtherCatDevice.h"
-#include "Fieldbus/Utilities/DS402.h"
+#include "Fieldbus/Utilities/DS402Axis.h"
 
 class KincoFD : public EtherCatDevice {
 public:
 	
 	DEFINE_ETHERCAT_DEVICE(KincoFD, "Kinco FD", "KincoFD", "Kinco", "Servo Drives", 0x681168, 0x464445)
 
+	std::shared_ptr<DS402Axis> axis;
+	
+	bool b_enable = false;
+	bool b_disable = false;
+	uint32_t pos = 0;
+	
 };
