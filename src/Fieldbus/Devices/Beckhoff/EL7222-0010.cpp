8c9cc4fab92d177e638feebb4724ca5d5d4507da
Leo Becker <leobecker3@gmail.com>
2025-03-06 09:36:18 +0100

    added ELM7231 error codes

--- src/Fieldbus/Devices/Beckhoff/EL7222-0010.cpp
+++ src/Fieldbus/Devices/Beckhoff/EL7222-0010.cpp
@@ -780,6 +780,10 @@ std::string ELM7231_9016::getDiagnosticsStringFromTextID(uint16_t textID){
 	switch(textID){
 		case 0x0000: return "No Error";
 		case 0x1201: return "Communication re-established";
+		case 0x2084: return "The %d. connection has received an invalid FSoE-CRC in state DATA";
+		case 0x2085: return "The FSoE-Watchdog of the connection has been expired in state DATA";
+		case 0x3400: return "The %d. group has no more errors";
+		case 0x3410: return "All groups will enter the ERROR-state because the safe logic program has been restarted";
 		case 0x4101: return "Terminal-Overtemperature";
 		case 0x4102: return "PDO-configuration is incompatible to the selected mode of operation";
 		case 0x4107: return "Undervoltage Up";
